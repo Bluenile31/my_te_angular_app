7742a9d56289ae1d6271ed0d0d874f1cc1a58f1f
Bluenile31 <nileshhosure47@gmail.com>
2022-09-27 16:18:12 +0530

    commit(27/09)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
-  production: false,
-  userapi:"http://localhost:3000/users",
-  cartapi:"http://localhost:3000/cart"
+  production: true,
+  //userapi:"http://localhost:3000/users",
+  //cartapi:"http://localhost:3000/cart"
+  postsapi:"https://heroku-json-server-fakedbb.herokuapp.com/posts",
+  userapi:"https://heroku-json-server-fakedbb.herokuapp.com/users",
+  cartapi:"https://heroku-json-server-fakedbb.herokuapp.com/cart"
 };
